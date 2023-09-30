fd3188d77177c7f8e6aebb0b2c8c5566c8533f27
Guilherme H. l. dos Santos <125158701+GuilhermeLimaSantos@users.noreply.github.com>
2023-09-30 16:18:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
-function exercicio1(){
-  var nota = parseInt(prompt("Digite um número"));
+function exercicio1() {
+  var nota = parseInt(prompt("Digite um numero"));
 
-  while(nota < 0 || nota > 10) {
+  while (nota < 0 || nota > 10) {
     nota = parseInt(prompt("Digite novamente a nota"));
+  }
+  document.getElementById("root").style.display = "block"
+  document.getElementById("root").innerHTML = `<p>A sua nota: ${nota}</p>`;
+}
+
+function exercicio2() {
+  const timerDiv = document.getElementById('root');
+  let timerCount = 10;
+  var cronometro = setInterval(() => {
+      timerDiv.innerHTML = `<p>${timerCount--}</p>`;
+    if (timerCount === 0) {
+      clearInterval(cronometro)
+        timerDiv.innerHTML = `<p> Feliz Ano Novo </p>`;
+      }
+  }, 1000);
+};
+
+function exercicio3() {
+  var teste = prompt("Nome");
+  var s = prompt("Senha");
+
+  while (teste === s) {
+    alert("Os campos estão iguais")
+    teste = prompt("Nome");
+    s = prompt("Senha");
   }
 
   document.getElementById("root").style.display = "block"
-  document.getElementById("root").innerHTML = '<p>A sua nota: ${nota}</p>'
+  document.getElementById("root").innerHTML = `Nome:${teste}<br> Senha:${s}`;
 }
 
-function exercicio2(){
-  var contagem = parseInt(prompt("Ano Novo"));
+function exercicio4 () {
+    var num1 = parseInt(prompt("digite um numero"))
+    var num2 = parseInt(prompt("digite outro numero"))
+    var resultado = num1 + num2
+    alert(resultado)
+    document.getElementById("root").style.display = "block"
+    document.getElementById("root").innerHTML = `
+    <P> sua soma ${num1} + ${num2} = ${resultado}</p>`
+}
 
-  while(contagem )
+function limpar() {
+  document.getElementById("root").style.display = "none"
+  exercicio4 = document.getElementById("exercicio4").style.display = "none"
 }
